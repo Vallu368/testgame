c7465ac7ad9f5b0c8ff3441f6240e3855416e8f0
vallu368 <vsavolainen22@gmail.com>
2023-12-23 02:28:26 +0200

    we back baybee

---
+++ Assets/Scripts/Player/PlayerStates/PlayerCombat.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public bool isAttacking = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
